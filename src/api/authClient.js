5215aca98fe8746d9959e638babf4c95d0238e4d
jigong2024 <choijming21@naver.com>
2024-09-09 18:13:29 +0900

    update: mbti 문항 생성

--- src/api/authClient.js
+++ src/api/authClient.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const authClient = axios.create({
   baseURL: "https://moneyfulpublicpolicy.co.kr",
 });
+
+export const resultClient = axios.create({
+  baseURL: "http://localhost:5000/testResults",
+});
---
+++ src/api/testResults.js
@@ -0,0 +1,23 @@
+import { resultClient } from "./authClient";
+
+export const getTestResults = async () => {
+  const response = await resultClient.get("/");
+  return response.data;
+};
+
+export const createTestResult = async (resultData) => {
+  const response = await resultClient.post("/", resultData);
+  return response.data;
+};
+
+export const deleteTestResult = async (id) => {
+  const response = await resultClient.delete(`/${id}`);
+  return response.data;
+};
+
+export const updateTestResult = async (id, visibility) => {
+  const response = await resultClient.patch(`/${id}`, {
+    visibility,
+  });
+  return response.data;
+};
